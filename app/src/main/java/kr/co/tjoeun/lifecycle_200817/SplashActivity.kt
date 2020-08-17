ff46ebdf74d8644f06669ca3e44433d6869f3cc1
이선영 <sunny_525@naver.com>
2020-08-17 20:57:33 +0900

    Splash 화면 추가 - 시작점으로 변경 + 액션바 제거

---
+++ app/src/main/java/kr/co/tjoeun/lifecycle_200817/SplashActivity.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.lifecycle_200817
+
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
