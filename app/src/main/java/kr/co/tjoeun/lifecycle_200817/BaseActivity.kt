9a0b4a01a328984e52d6100957b1cc52b3600881
이선영 <sunny_525@naver.com>
2020-08-17 21:43:26 +0900

    2.5초가 지난 후 메인 화면으로 이동하도록 Handler 적용_(수정함)

--- app/src/main/java/kr/co/tjoeun/lifecycle_200817/BaseActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_200817/BaseActivity.kt
@@ -10,6 +10,4 @@ abstract class BaseActivity : AppCompatActivity() {
 
     abstract fun setValues()
 
-
-
 }
--- app/src/main/java/kr/co/tjoeun/lifecycle_200817/SplashActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_200817/SplashActivity.kt
@@ -6,9 +6,12 @@ import android.os.Handler
 import android.os.Looper
 
 class SplashActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setUpEvents()
+        setValues()
     }
 
     override fun setUpEvents() {
@@ -17,18 +20,18 @@ class SplashActivity : BaseActivity() {
 
     override fun setValues() {
 
-//        2.5(2500ms)초는 로딩화면을 보여주고 나서 메인으로 이동하고 싶다.
+//        2.5 초 (== 2500ms)는 로딩화면을 보여주고 나서 메인으로 이동하고싶다.
 
         val myHandler = Handler(Looper.getMainLooper())
 
         myHandler.postDelayed({
 //            일정 시간이 지난 후에 실행시키고자 하는 코드 작성
-    //        Splash 화면 => 메인 화면으로 (자동으로) 이동해야 함.
+            //        Splash 화면 => 메인화면으로 (자동으로) 이동해야함.
 
             val myIntent = Intent(mContext, MainActivity::class.java)
             startActivity(myIntent)
 
-//        메인 화면으로 이동한 후에는 종료
+//        메인화면으로 이동한 후에는 종료
             finish()
 
         }, 2500)
@@ -36,6 +39,5 @@ class SplashActivity : BaseActivity() {
 
 
     }
-
 
 }
