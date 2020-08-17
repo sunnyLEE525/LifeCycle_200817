5b3ab3d43535a0019cee61e34d82430762c63768
이선영 <sunny_525@naver.com>
2020-08-17 19:47:48 +0900

    다른 화면을 추가하여 다른 화면을  Main으로 만듬 -> 잘 안됨..ㅠㅠ

--- app/src/main/java/kr/co/tjoeun/lifecycle_200817/OtherActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_200817/OtherActivity.kt
@@ -1,25 +1,18 @@
 package kr.co.tjoeun.lifecycle_200817
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
+class OtherActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_other)
         setUpEvents()
         setValues()
-    }
 
+}
     override fun setUpEvents() {
-
-        otherActivityBtn.setOnClickListener {
-            val myIntent = Intent(mContext, OtherActivity::class.java)
-            startActivity(myIntent)
-        }
 
     }
 
