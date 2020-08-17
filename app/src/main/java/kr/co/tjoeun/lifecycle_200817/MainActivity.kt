8c4b7bbc69a393bdf4ef8d4194cae00e03b156a8
이선영 <sunny_525@naver.com>
2020-08-17 20:50:17 +0900

    생명주기 관련 함수 테스트

--- app/src/main/java/kr/co/tjoeun/lifecycle_200817/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_200817/MainActivity.kt
@@ -16,6 +16,23 @@ class MainActivity : BaseActivity() {
         Log.d("메인화면","onCreate 실행됨")
     }
 
+//    다른 화면에 갔다가 올 때마다 계속 실행됨.
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("메인화면", "OnResume 실행됨 - 화면이 나타남")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("메인화면", "onPause 실행됨 - 화면이 사라짐")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("메인화면", "onDestroy 실행됨 - 액티비티가 메모리에서 소멸됨")
+    }
+
     override fun setUpEvents() {
 
         otherActivityBtn.setOnClickListener {
