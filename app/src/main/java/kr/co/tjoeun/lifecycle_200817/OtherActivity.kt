0808f70778231f88f83afb3e7db96d2bf29b1e17
이선영 <sunny_525@naver.com>
2020-08-19 19:21:29 +0900

    메인으로 이동하기 추가_1

--- app/src/main/java/kr/co/tjoeun/lifecycle_200817/OtherActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_200817/OtherActivity.kt
@@ -16,14 +16,12 @@ class OtherActivity : BaseActivity() {
 
 }
     override fun setUpEvents() {
-
-    }
-
-    override fun setValues() {
-
         backToMainBtn.setOnClickListener {
             finish()
         }
+    }
+
+    override fun setValues() {
 
     }
 
